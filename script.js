a6f84f17323c9e6535c09fe7b728970bfac0baed
Ryan Nix <rnix@redhat.com>
2021-08-25 16:07:57 -0500

    Small change

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const quizData = [
         question: "Which language runs in a web browser?",
         a: "Java",
         b: "C",
-        c: "French",
+        c: "Python",
         d: "JavaScript",
         correct: "d",
     },
